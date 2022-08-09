19096cce583a422767ac421d4a5cd65fd643ec73
Fuglyuck <harrisoncwalton@gmail.com>
2022-08-09 15:55:24 +0100

    More Safari Updates
    Removed Clip Path as Safari 15 doesn't like that

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,14 +4,7 @@ import styles from "./Hero.module.scss";
 const Hero = ({ src }) => {
   return (
     <div className={styles.heroContainer}>
-      <video
-        playsInline
-        className={styles.hero}
-        muted
-        loop
-        autoPlay
-        style={{ clipPath: "url(#clipper)" }}
-      >
+      <video playsInline className={styles.hero} muted loop autoPlay>
         <source src="/videos/Pink_n_purple_video.mp4" type="video/mp4" />
       </video>
     </div>
