be38ea39130fed55ff199e3746696f6b495f9702
Fuglyuck <harrisoncwalton@gmail.com>
2022-09-02 16:32:23 +0100

    Updated Labels in form
    Added `htmlFor` in form labels for accessibility

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -27,16 +27,26 @@ const Contact = () => {
 
       <div className={styles.formContainer}>
         <form ref={form} className={styles.form} onSubmit={sendEmail}>
-          <label>Name:</label>
-          <input type="text" name="user_name" placeholder="Name"></input>
-          <label>Email:</label>
-          <input type="email" name="user_email" placeholder="Email"></input>
-          <label>Mobile:</label>
-          <input type="mobile" name="user_mobile" placeholder="Mobile"></input>
-          <label>Location:</label>
-          <input type="location" name="user_location" placeholder="Location"></input>
-          <label>Type of Event:</label>
-          <select type="event-type" name="user_event-type">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="user_name" placeholder="Name"></input>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="user_email" placeholder="Email"></input>
+          <label htmlFor="mobile">Mobile:</label>
+          <input
+            type="mobile"
+            id="mobile"
+            name="user_mobile"
+            placeholder="Mobile"
+          ></input>
+          <label htmlFor="location">Location:</label>
+          <input
+            type="location"
+            id="location"
+            name="user_location"
+            placeholder="Location"
+          ></input>
+          <label htmlFor="event">Type of Event:</label>
+          <select type="event-type" id="event" name="user_event-type">
             <option value="Nothing">None</option>
             <option value="a Party">Parties</option>
             <option value="a Corporate event">Corporate</option>
@@ -44,13 +54,13 @@ const Contact = () => {
             <option value="a Christening">Christening</option>
             <option value="Giggles on Wheels">Giggles on Wheels</option>
           </select>
-          <label>Preferred contact method:</label>
-          <select type="contact-method" name="user_contact-method">
+          <label htmlFor="method">Preferred contact method:</label>
+          <select type="contact-method" id="method" name="user_contact-method">
             <option value="mobile">Mobile</option>
             <option value="email">Email</option>
           </select>
-          <label>Query:</label>
-          <textarea name="message"></textarea>
+          <label htmlFor="query">Query:</label>
+          <textarea name="message" id="query"></textarea>
           <button type="submit">Submit</button>
         </form>
       </div>
