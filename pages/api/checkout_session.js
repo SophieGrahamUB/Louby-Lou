e4a27f23d26c71f8ac84e672028b3b35fe474088
fuglyuckDev <harrisoncwalton@gmail.com>
2023-04-21 20:55:12 +0100

    altered events
    removed patricroft
    placed in checkout session .js

--- pages/api/checkout_session.js
+++ pages/api/checkout_session.js
@@ -32,3 +32,18 @@ export default async function handler(req, res) {
     res.status(405).end("Method Not Allowed");
   }
 }
+
+// {
+//   "id": 3,
+//   "attributes": {
+//     "Name": "Patricroft",
+//     "Date": "03/05/2023",
+//     "Adult": 0,
+//     "Child": 8.5,
+//     "length": 3,
+//     "Location": "M30 7AW",
+//     "time": "18:30:00.000",
+//     "eventImageURL": "/images/events/patricroft-03-05-23.jpg",
+//     "eventDescription": "Come & join us for our Fun For All Party, open to all ages at @patricroftconsclub! The disco will be held on Wednesday 3rd May, and we'll be celebrating for 3 hours from 6:30pm🥳  Feel free to come & go as you please throughout the night, you and your children don't want to miss out on the dancing, magic & giggles 🤩 There might even be a few prizes to be won too👀  Tickets are £8.50 per child and if you buy 3 tickets, you get 1 free, so make sure to bring all your friends!"
+//   }
+// },
